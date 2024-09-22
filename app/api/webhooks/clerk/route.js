319c9df6230724881b48703114bad658177bc2b1
primevx <josefyitayal@gmail.com>
2024-09-22 19:35:43 +0300

    i think clerk is working

--- app/api/webhooks/clerk/route.js
+++ app/api/webhooks/clerk/route.js
@@ -9,8 +9,8 @@ import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 
 export async function POST(req) {
     // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
+    console.log("================== post request =====================")
     const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-    console.log("================== post request ===================")
     if (!WEBHOOK_SECRET) {
         throw new Error(
             "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
